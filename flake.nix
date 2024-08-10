1c54c6e2921f6240b6f8171d53d3bde8931ddbca
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2024-08-10 21:04:28 +0800

    init: add project flake

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    flake-utils,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = devenv.lib.mkShell {
+        inherit inputs pkgs;
+        modules = [
+          {
+            name = "work";
+            languages = {
+              nix.enable = true;
+            };
+            packages = with pkgs;
+              [
+                alejandra
+              ]
+              ++ (with pkgs.python3Packages; [
+                python-lsp-server
+                pylint
+                mypy
+              ]);
+          }
+        ];
+      };
+    });
+}
