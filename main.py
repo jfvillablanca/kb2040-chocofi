54e26cb69cc55d04f1b6f812453217174566dd70
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2024-08-11 13:37:45 +0800

    feat: add initial working main.py
    - remove all rgb related settings
    - split side is dynamically inferred based on the storage mount name
    - replace boilerplate layers with qwerty, nav, number, symbol, and
      function

--- main.py
+++ main.py
@@ -1,67 +1,74 @@
+from storage import getmount
+
 from kb import KMKKeyboard
-from kmk.extensions.rgb import RGB
 from kmk.keys import KC
+from kmk.modules.holdtap import HoldTap
 from kmk.modules.layers import Layers
 from kmk.modules.split import Split, SplitSide, SplitType
 
 keyboard = KMKKeyboard()
 
-# Adding extensions
-rgb = RGB(pixel_pin=keyboard.rgb_pixel_pin, num_pixels=27, val_limit=100, hue_default=190, sat_default=100, val_default=5)
-
-# TODO Comment one of these on each side
-split_side = SplitSide.LEFT
-split_side = SplitSide.RIGHT
-split = Split(split_type=SplitType.BLE, split_side=split_side)
+side = SplitSide.RIGHT if str(getmount('/').label)[-1] == 'R' else SplitSide.LEFT
+split = Split(split_type=SplitType.UART, split_side=side, use_pio=True, uart_flip=True)
 
 layers = Layers()
+holdtap = HoldTap()
 
-keyboard.modules = [layers, split]
-keyboard.extensions = [rgb]
-#
-# Cleaner key names
+keyboard.modules = [layers, holdtap, split]
+
 _______ = KC.TRNS
 XXXXXXX = KC.NO
 
-LOWER = KC.MO(1)
-RAISE = KC.MO(2)
-ADJUST = KC.LT(3, KC.SPC)
+ESCAPE = KC.HT(KC.ESC, KC.LGUI) 
 
-RGB_TOG = KC.RGB_TOG
-RGB_HUI = KC.RGB_HUI
-RGB_HUD = KC.RGB_HUI
-RGB_SAI = KC.RGB_SAI
-RGB_SAD = KC.RGB_SAD
-RGB_VAI = KC.RGB_VAI
-RGB_VAD = KC.RGB_VAD
-
+NAV = KC.LT(2, KC.TAB)
+SYMBOL = KC.LT(3, KC.ENT) 
+BSPC = KC.LT(4, KC.BSPC)
 
 # fmt:off
 keyboard.keymap = [
-    [  #QWERTY
-        KC.TAB,    KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,                         KC.Y,    KC.U,    KC.I,    KC.O,   KC.P,  KC.BSPC,\
-        KC.LCTL,   KC.A,    KC.S,    KC.D,    KC.F,    KC.G,                         KC.H,    KC.J,    KC.K,    KC.L, KC.SCLN, KC.QUOT,\
-        KC.LSFT,   KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,                         KC.N,    KC.M, KC.COMM,  KC.DOT, KC.SLSH, KC.RSFT,\
-                                            KC.LGUI,   LOWER,  ADJUST,     KC.ENT,   RAISE,  KC.RALT,
+    [  # 0: BASE
+        KC.Q,    KC.W,    KC.F,    KC.P,    KC.G,                            KC.J,    KC.L,    KC.U,    KC.Y, KC.SCLN,\
+        KC.A,    KC.R,    KC.S,    KC.T,    KC.D,                            KC.H,    KC.N,    KC.E,    KC.I,    KC.O,\
+        KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,                            KC.K,    KC.M, KC.COMM,  KC.DOT, KC.SLSH,\
+                                   ESCAPE,  BSPC,    NAV,         KC.SPC,  SYMBOL, KC.RSFT,
     ],
-    [  #LOWER
-        KC.ESC,   KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,                         KC.N6,   KC.N7,  KC.N8,   KC.N9,   KC.N0, KC.BSPC,\
-        KC.LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        KC.LEFT, KC.DOWN, KC.UP,   KC.RIGHT, XXXXXXX, XXXXXXX,\
-        KC.LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
-                                            KC.LGUI,   LOWER,  ADJUST,     KC.ENT,   RAISE,  KC.RALT,
+    [  # 1. QWERTY
+        KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,                            KC.Y,    KC.U,    KC.I,    KC.O,    KC.P,\
+        KC.A,    KC.S,    KC.D,    KC.F,    KC.G,                            KC.H,    KC.J,    KC.K,    KC.L, KC.SCLN,\
+        KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,                            KC.N,    KC.M, KC.COMM,  KC.DOT, KC.SLSH,\
+                                   _______, _______, _______,    _______, _______, _______,
     ],
-    [  #RAISE
-        KC.ESC, KC.EXLM,   KC.AT, KC.HASH,  KC.DLR, KC.PERC,                         KC.CIRC, KC.AMPR, KC.ASTR, KC.LPRN, KC.RPRN, KC.BSPC,\
-        KC.LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        KC.MINS,  KC.EQL, KC.LCBR, KC.RCBR, KC.PIPE,  KC.GRV,\
-        KC.LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        KC.UNDS, KC.PLUS, KC.LBRC, KC.RBRC, KC.BSLS, KC.TILD,\
-                                            KC.LGUI,   LOWER,  ADJUST,     KC.ENT,   RAISE,  KC.RALT,
+    [  # 2. NAVIGATION
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+        KC.LGUI, KC.LALT, KC.LSFT, KC.LCTL, XXXXXXX,                      KC.LEFT, KC.DOWN,   KC.UP, KC.RGHT, XXXXXXX,\
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+                                   XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,
     ],
-    [  #ADJUST
-        RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
-        XXXXXXX, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
-                                            KC.LGUI,   LOWER,  ADJUST,     KC.ENT,   RAISE,  KC.RALT,
-    ]
+    [  # 3. SYMBOL
+        KC.EQL,  KC.AMPR, KC.ASTR, KC.LPRN, KC.RPRN,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+        KC.MINS, KC.DLR,  KC.PERC, KC.CIRC, KC.PLUS,                      XXXXXXX, KC.RCTL, KC.RSFT, KC.RALT, KC.RGUI,\
+        KC.GRV,  KC.EXLM, KC.AT,   KC.HASH, KC.BSLS,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+                                   KC.LBRC, KC.RBRC, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,
+    ],
+    [  # 4. NUMBER
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,   KC.N7,   KC.N8,   KC.N9, XXXXXXX,\
+        KC.LGUI, KC.LALT, KC.LSFT, KC.LCTL, XXXXXXX,                      XXXXXXX,   KC.N4,   KC.N5,   KC.N6, XXXXXXX,\
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,   KC.N1,   KC.N2,   KC.N3, XXXXXXX,\
+                                   XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,  KC.DOT,   KC.N0,
+    ],
+    [  # 5. FUNCTION
+        KC.F12,  KC.F7,   KC.F8,   KC.F9,   XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+        KC.F11,  KC.F4,   KC.F5,   KC.F6,   XXXXXXX,                      XXXXXXX, KC.RCTL, KC.RSFT, KC.RALT, KC.RGUI,\
+        KC.F10,  KC.F1,   KC.F2,   KC.F3,   XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+                                   XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,
+    ],
+    # [  # BLANK
+    #     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+    #     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+    #     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+    #                                XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX,
+    # ],
 ]
 # fmt:on
 
