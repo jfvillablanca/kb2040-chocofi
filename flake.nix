3d13e452b1271321066ae871449227aed4d08376
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2024-08-24 09:52:26 +0800

    chore: add mpy-cross
    - the `mpy-cross` binary doesn't work out of the box since it's
      a dynamically linked executable.
    - nix-alien partially solves the issue. If I run `nix-alien mpy-cross`
      it will be able to run `mpy-cross`

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     devenv.url = "github:cachix/devenv";
+    nix-alien.url = "github:thiagokokada/nix-alien";
   };
 
   outputs = {
@@ -10,12 +11,39 @@
     nixpkgs,
     devenv,
     flake-utils,
+    nix-alien,
     ...
   } @ inputs:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {
         inherit system;
+        overlays = [
+          nix-alien.overlays.default
+        ];
       };
+      mpy-cross =
+        pkgs.stdenv.mkDerivation
+        {
+          name = "mpy-cross";
+          src = pkgs.fetchurl {
+            url = "https://adafruit-circuit-python.s3.amazonaws.com/bin/mpy-cross/linux-amd64/mpy-cross-linux-amd64-9.1.1.static";
+            hash = "sha256-i865idypRM/7HYMWg80zLTc/mztzdDjycuFH1Y1JkcY=";
+          };
+
+          phases = ["installPhase"];
+
+          installPhase = ''
+            runHook preInstall
+
+            mkdir -p $out/bin
+            cp $src $out/bin/mpy-cross
+            chmod +x $out/bin/mpy-cross
+
+            runHook postInstall
+          '';
+
+          meta.mainProgram = "mpy-cross";
+        };
     in {
       devShells.default = devenv.lib.mkShell {
         inherit inputs pkgs;
@@ -34,7 +62,11 @@
                 python-lsp-server
                 pylint
                 mypy
-              ]);
+              ])
+              ++ [
+                mpy-cross
+                pkgs.nix-alien
+              ];
           }
         ];
       };
